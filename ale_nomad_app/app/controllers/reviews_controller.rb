bc2595206eb9bfac2c830544180f55d6581d9ede
keilsmit <keilsmit@yahoo.com>
2016-12-11 18:09:16 -0500

    added rake fil and fix brew model

--- ale_nomad_app/app/controllers/reviews_controller.rb
+++ ale_nomad_app/app/controllers/reviews_controller.rb
@@ -8,12 +8,10 @@ class ReviewsController < ApplicationController
           render json: @user.errors.full_messages, status: 422
         end
   end
-
   def show
     @review = Review.find(params[:id])
     render json: @review
   end
-
   def update
     @review = Review.find_by(token: params[:token])
     @review.update!(update_review_params)
@@ -23,8 +21,6 @@ class ReviewsController < ApplicationController
         render json: @review.errors
       end
   end
-
-
   def my_ratings
     @my_ratings = current_user.reviews.order("created_at DESC")
     render json: @my_ratings
