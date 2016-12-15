5a480b508fc66f74d73a8bd8caba2dd1bdfa0a30
Jeff Bumgardner <bumgardner.jeffrey@gmail.com>
2016-12-15 10:03:21 -0500

    removed more junk code

--- webpack/FindBeer.js
+++ webpack/FindBeer.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router'
 import Filter from './Filter'
-import alt from './lib/alt'
 
-// TODO: comment code so others can read it.
 
 
 class FindBeer extends React.Component {
     constructor(props) {
         super(props)
     }
-    
+
 
     render(){
         return (
